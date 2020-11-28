18c683467afa2d73f5ecf6d0ce4cdc993ed1ed68
irobii <kakyung.kim@gmail.com>
2020-11-28 16:16:36 +0900

    commit

---
+++ R/covidSupp.R
@@ -0,0 +1,14 @@
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 4273 rows and 7 variables:
+#' \describe{
+#'   \item{\code{no}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{region}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{startup.year}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{year}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{month}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{loan.krw1mil}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{industry}}{integer COLUMN_DESCRIPTION}
+#'}
+#' @details DETAILS
+"covidSupp"
